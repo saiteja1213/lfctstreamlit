090d68e2042ca36afb2dcfe3cec4d79fffd11365
Saiteja Gangavaram <69451084+saiteja1213@users.noreply.github.com>
2026-03-12 16:24:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,22 +6,4 @@ st.set_page_config(page_title="LFxCT season2", layout="wide")
 
 st.title("LFxCT Season 2🏏")
 
-PRED_FILE = "predictions.csv"
-
-# Create predictions file only if it does not exist
-if not os.path.exists(PRED_FILE):
-
-    df = pd.DataFrame(columns=[
-        "timestamp",
-        "username",
-        "match",
-        "prediction",
-        "bold",
-        "approval_status",
-        "result_status",
-        "score"
-    ])
-
-    df.to_csv(PRED_FILE, index=False)
-
 st.write("Use the sidebar to navigate through the pages")
