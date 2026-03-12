99a70492e9ed26dc5b5a2e9c662670330a24f96d
saiteja1213 <69451084+saiteja1213@users.noreply.github.com>
2026-03-12 13:47:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.set_page_config(page_title="Prediction League", layout="wide")
-st.title("Prediction League Dashboard")
-st.write("Use the sidebar to navigate pages.")
+st.set_page_config(page_title="LFxCT", layout="wide")
+st.title("LFxCT Season 2")
+st.write("Use the side bar to navigate between pages.")
