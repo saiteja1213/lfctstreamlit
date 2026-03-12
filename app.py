10d3226baa2aeba60e07a3698d7b7fd6009ad48f
saiteja1213 <69451084+saiteja1213@users.noreply.github.com>
2026-03-12 13:58:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,27 @@
 import streamlit as st
+import os
+import pandas as pd
 
-st.set_page_config(page_title="LFxCT", layout="wide")
-st.title("LFxCT Season 2")
-st.write("Use the side bar to navigate between pages.")
+st.set_page_config(page_title="Prediction League", layout="wide")
+
+st.title("🏏 IPL Prediction League")
+
+PRED_FILE = "predictions.csv"
+
+# Create predictions file only if it does not exist
+if not os.path.exists(PRED_FILE):
+
+    df = pd.DataFrame(columns=[
+        "timestamp",
+        "username",
+        "match",
+        "prediction",
+        "bold",
+        "approval_status",
+        "result_status",
+        "score"
+    ])
+
+    df.to_csv(PRED_FILE, index=False)
+
+st.write("Use the sidebar to navigate.")
