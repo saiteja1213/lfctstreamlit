2cfc1713d3d9b08b94571976e649cbac049598f8
Saiteja Gangavaram <69451084+saiteja1213@users.noreply.github.com>
2026-03-12 14:54:39 -0400

    Create gsheets.py

---
+++ gsheets.py
@@ -0,0 +1,35 @@
+import gspread
+import pandas as pd
+from oauth2client.service_account import ServiceAccountCredentials
+
+SHEET_NAME = "LFxCT Scoreboard"
+
+scope = [
+    "https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/drive"
+]
+
+creds = ServiceAccountCredentials.from_json_keyfile_name(
+    "service_account.json",
+    scope
+)
+
+client = gspread.authorize(creds)
+
+def get_sheet():
+    sheet = client.open(SHEET_NAME).sheet1
+    return sheet
+
+def read_data():
+    sheet = get_sheet()
+    data = sheet.get_all_records()
+    df = pd.DataFrame(data)
+    return df
+
+def append_row(row):
+    sheet = get_sheet()
+    sheet.append_row(row)
+
+def update_cell(row, col, value):
+    sheet = get_sheet()
+    sheet.update_cell(row, col, value)
